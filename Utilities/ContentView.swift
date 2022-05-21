c5cc5934c0d0a284c799f25fa6182c6e259092a0
Natalia Lysakova <natashalysakova@gmail.com>
2022-05-22 02:04:51 +0300

    Initial Commit

---
+++ Utilities/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Utilities
+//
+//  Created by Natalya Lysakova on 22.05.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Utilities/UtilitiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  UtilitiesApp.swift
+//  Utilities
+//
+//  Created by Natalya Lysakova on 22.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct UtilitiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
